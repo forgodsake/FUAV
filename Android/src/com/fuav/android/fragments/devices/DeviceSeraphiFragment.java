cd2b7643a3c5ee53e74d6a1b208c869571b535a7
one <411156176@qq.com>
2016-02-01 18:12:11 +0800

    some changes

--- Android/src/com/fuav/android/fragments/devices/DeviceSeraphiFragment.java
+++ Android/src/com/fuav/android/fragments/devices/DeviceSeraphiFragment.java
@@ -2,16 +2,20 @@ package com.fuav.android.fragments.devices;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.fuav.android.R;
 import com.fuav.android.activities.FlightActivity;
+import com.fuav.android.activities.helpers.SuperUI;
+import com.fuav.android.fragments.helpers.ApiListenerFragment;
+import com.o3dr.android.client.Drone;
 
 
-public class DeviceSeraphiFragment extends Fragment {
+public class DeviceSeraphiFragment extends ApiListenerFragment implements View.OnClickListener{
+
+
 
     public DeviceSeraphiFragment() {
         // Required empty public constructor
@@ -23,16 +27,37 @@ public class DeviceSeraphiFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_device_seraphi, container, false);
-        view.findViewById(R.id.imageViewLogo).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), FlightActivity.class));
-            }
-        });
         return view;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.imageViewLogo).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.imageViewLogo:
+                ((SuperUI) getActivity()).toggleDroneConnection();
+                Drone drone=getDrone();
+//                if(drone.isConnected()) {
+                    startActivity(new Intent(getActivity(), FlightActivity.class));
+//                }
+                break;
+        }
+    }
 
 
+    @Override
+    public void onApiConnected() {
+        
+    }
 
+    @Override
+    public void onApiDisconnected() {
+
+    }
 }
