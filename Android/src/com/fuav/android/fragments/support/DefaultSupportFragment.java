d8ce76d7e580ad0330470367e2c24687e2d134c4
god <411156176@qq.com>
2016-03-28 00:33:20 +0800

    add share function and bug fix

--- Android/src/com/fuav/android/fragments/support/DefaultSupportFragment.java
+++ Android/src/com/fuav/android/fragments/support/DefaultSupportFragment.java
@@ -1,6 +1,7 @@
 package com.fuav.android.fragments.support;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SimpleAdapter;
 
 import com.fuav.android.R;
@@ -21,6 +23,10 @@ import com.fuav.android.view.CustListView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.onekeyshare.OnekeyShareTheme;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,6 +45,7 @@ public class DefaultSupportFragment extends Fragment {
     private SimpleAdapter adapter;
     private List<HashMap<String, Object>> list;
     private Button btn_register,btn_login;
+    private ImageView img_share;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,12 +55,15 @@ public class DefaultSupportFragment extends Fragment {
         supportList = (CustListView) view.findViewById(R.id.listViewSupport);
         btn_register = (Button) view.findViewById(R.id.btn_register);
         btn_login = (Button) view.findViewById(R.id.btn_login);
+        img_share = (ImageView) view.findViewById(R.id.img_share);
         return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        // 初始化ShareSDK
+        ShareSDK.initSDK(getActivity());
         list = new ArrayList<HashMap<String,Object>>();
         for (int i = 0; i < images.length; i++) {
             HashMap<String, Object> map = new HashMap<String, Object>();
@@ -111,8 +121,79 @@ public class DefaultSupportFragment extends Fragment {
                 startActivity(new Intent(getActivity(), LoginActivity.class));
             }
         });
+
+        img_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showShare(getActivity(),null,true);
+            }
+        });
+
     }
 
+    /**
+     * 演示调用ShareSDK执行分享
+     *
+     * @param context
+     * @param platformToShare  指定直接分享平台名称（一旦设置了平台名称，则九宫格将不会显示）
+     * @param showContentEdit  是否显示编辑页
+     */
+    public static void showShare(Context context, String platformToShare, boolean showContentEdit) {
+        OnekeyShare oks = new OnekeyShare();
+        oks.setSilent(!showContentEdit);
+        if (platformToShare != null) {
+            oks.setPlatform(platformToShare);
+        }
+        //ShareSDK快捷分享提供两个界面第一个是九宫格 CLASSIC  第二个是SKYBLUE
+        oks.setTheme(OnekeyShareTheme.CLASSIC);
+        // 令编辑页面显示为Dialog模式
+        oks.setDialogMode();
+        // 在自动授权时可以禁用SSO方式
+        oks.disableSSOWhenAuthorize();
+        //oks.setAddress("12345678901"); //分享短信的号码和邮件的地址
+        oks.setTitle("FUAV");
+        oks.setTitleUrl("http://www.fuav.com");
+        oks.setText("FUAV");
+        //oks.setImagePath("/sdcard/test-pic.jpg");  //分享sdcard目录下的图片
+//        oks.setImageUrl("http://");
+        oks.setUrl("http://www.fuav.com"); //微信不绕过审核分享链接
+        //oks.setFilePath("/sdcard/test-pic.jpg");  //filePath是待分享应用程序的本地路劲，仅在微信（易信）好友和Dropbox中使用，否则可以不提供
+        oks.setComment("分享"); //我对这条分享的评论，仅在人人网和QQ空间使用，否则可以不提供
+        oks.setSite("FUAV");  //QZone分享完之后返回应用时提示框上显示的名称
+        oks.setSiteUrl("http://www.fuav.com");//QZone分享参数
+        oks.setVenueName("ShareSDK");
+        oks.setVenueDescription("This is a beautiful place!");
+        // 将快捷分享的操作结果将通过OneKeyShareCallback回调
+        //oks.setCallback(new OneKeyShareCallback());
+        // 去自定义不同平台的字段内容
+        //oks.setShareContentCustomizeCallback(new ShareContentCustomizeDemo());
+        // 在九宫格设置自定义的图标
+//        Bitmap logo = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher);
+//        String label = "ShareSDK";
+//        View.OnClickListener listener = new View.OnClickListener() {
+//            public void onClick(View v) {
+//
+//            }
+//        };
+//        oks.setCustomerLogo(logo, label, listener);
+
+        // 为EditPage设置一个背景的View
+        //oks.setEditPageBackground(getPage());
+        // 隐藏九宫格中的新浪微博
+        // oks.addHiddenPlatform(SinaWeibo.NAME);
+
+        // String[] AVATARS = {
+        // 		"http://99touxiang.com/public/upload/nvsheng/125/27-011820_433.jpg",
+        // 		"http://img1.2345.com/duoteimg/qqTxImg/2012/04/09/13339485237265.jpg",
+        // 		"http://diy.qqjay.com/u/files/2012/0523/f466c38e1c6c99ee2d6cd7746207a97a.jpg",
+        // 		"http://diy.qqjay.com/u2/2013/0422/fadc08459b1ef5fc1ea6b5b8d22e44b4.jpg",
+        // 		"http://img1.2345.com/duoteimg/qqTxImg/2012/04/09/13339510584349.jpg",
+        // 		"http://diy.qqjay.com/u2/2013/0401/4355c29b30d295b26da6f242a65bcaad.jpg" };
+        // oks.setImageArray(AVATARS);              //腾讯微博和twitter用此方法分享多张图片，其他平台不可以
+
+        // 启动分享
+        oks.show(context);
+    }
 
 
 }
